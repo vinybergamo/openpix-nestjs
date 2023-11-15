2849a6b3805388727d6e8ba23e1dd0977f5bd766
Vinny <vinybergamo@gmail.com>
2023-11-15 11:30:03 -0300

    feat: return customerService and subscriptionService in openpixService

--- lib/services/openpix.service.ts
+++ lib/services/openpix.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { WebhookService } from './webhook.service';
 import { ChargeService } from './charge.service';
+import { SubscriptionService } from './subscription.service';
+import { CustomerService } from './customer.service';
 
 @Injectable()
 export class OpenPixService {
   constructor(
     private readonly webhookService: WebhookService,
     private readonly chargeService: ChargeService,
+    private readonly subscriptionService: SubscriptionService,
+    private readonly customerService: CustomerService,
   ) {}
 
   public get webhook() {
@@ -15,5 +19,13 @@ export class OpenPixService {
 
   public get charge() {
     return this.chargeService;
+  }
+
+  public get subscription() {
+    return this.subscriptionService;
+  }
+
+  public get customer() {
+    return this.customerService;
   }
 }
