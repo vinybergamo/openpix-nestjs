d74568b8819a4f350ab8ea2562d76e7c4520f5cb
vinybergamo <vinybergamo@gmail.com>
2025-02-02 14:52:09 -0300

    feat: add OpenPix module options and async registration support

--- lib/interfaces/openpix-module.interface.ts
+++ lib/interfaces/openpix-module.interface.ts
@@ -1,4 +1,28 @@
-export type OpenpixRegisterOption = {
+import {
+  FactoryProvider,
+  ModuleMetadata,
+  Provider,
+  Type,
+} from '@nestjs/common';
+
+export interface OpenPixModuleOptions {
   appId?: string;
   version?: 'v1';
-};
+  global?: boolean;
+}
+
+export interface OpenPixOptionsFactory {
+  createOpenPixOptions(): Promise<OpenPixModuleOptions> | OpenPixModuleOptions;
+}
+
+export interface OpenPixModuleAsyncOptions
+  extends Pick<ModuleMetadata, 'imports'> {
+  useExisting?: Type<OpenPixOptionsFactory>;
+  useClass?: Type<OpenPixOptionsFactory>;
+  useFactory?: (
+    ...args: any[]
+  ) => Promise<OpenPixModuleOptions> | OpenPixModuleOptions;
+  inject?: FactoryProvider['inject'];
+  extraProviders?: Provider[];
+  global?: boolean;
+}
--- lib/openpix.module.ts
+++ lib/openpix.module.ts
@@ -1,25 +1,100 @@
-import { Module } from '@nestjs/common';
-import { OpenpixRegisterOption } from './interfaces';
+import { DynamicModule, Module, Provider } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { OpenPixService } from './services/openpix.service';
 import { providers } from './providers';
+import {
+  OpenPixModuleAsyncOptions,
+  OpenPixModuleOptions,
+  OpenPixOptionsFactory,
+} from './interfaces';
+import { OPENPIX_MODULE_ID, OPENPIX_MODULE_OPTIONS } from './openpix.constants';
+import { randomStringGenerator } from '@nestjs/common/utils/random-string-generator.util';
 
 @Module({})
 export class OpenPixModule {
-  static register({ appId, version = 'v1' }: OpenpixRegisterOption) {
+  static register(options: OpenPixModuleOptions): DynamicModule {
+    const { appId, version, global } = options;
     return {
       module: OpenPixModule,
+      global: global,
       imports: [
         HttpModule.register({
-          baseURL: `https://api.openpix.com.br/api/${version}`,
+          baseURL: `https://api.openpix.com.br/api/${version ?? 'v1'}`,
           headers: {
             Authorization: `${appId}`,
             'Content-Type': 'application/json',
           },
         }),
       ],
-      providers,
+      providers: [
+        ...providers,
+        {
+          provide: OPENPIX_MODULE_ID,
+          useValue: randomStringGenerator(),
+        },
+      ],
       exports: [OpenPixService],
     };
+  }
+
+  static registerAsync(options: OpenPixModuleAsyncOptions): DynamicModule {
+    return {
+      module: OpenPixModule,
+      global: options.global,
+      imports: [
+        HttpModule.registerAsync({
+          useFactory: async (openPixOptions: OpenPixModuleOptions) => {
+            const { appId, version } = openPixOptions;
+            return {
+              baseURL: `https://api.openpix.com.br/api/${version ?? 'v1'}`,
+              headers: {
+                Authorization: `${appId}`,
+                'Content-Type': 'application/json',
+              },
+            };
+          },
+        }),
+      ],
+      providers: [
+        ...this.createAsyncProviders(options),
+        ...providers,
+        {
+          provide: OPENPIX_MODULE_ID,
+          useValue: randomStringGenerator(),
+        },
+      ],
+      exports: [OpenPixService],
+    };
+  }
+
+  private static createAsyncProviders(
+    options: OpenPixModuleAsyncOptions,
+  ): Provider[] {
+    if (options.useFactory) {
+      return [
+        {
+          provide: OPENPIX_MODULE_OPTIONS,
+          useFactory: options.useFactory,
+          inject: options.inject || [],
+        },
+      ];
+    }
+
+    return [
+      {
+        provide: OPENPIX_MODULE_OPTIONS,
+        useFactory: async (optionsFactory: OpenPixOptionsFactory) =>
+          await optionsFactory.createOpenPixOptions(),
+        inject: [options.useExisting || options.useClass],
+      },
+      ...(options.useClass
+        ? [
+            {
+              provide: options.useClass,
+              useClass: options.useClass,
+            },
+          ]
+        : []),
+    ];
   }
 }
