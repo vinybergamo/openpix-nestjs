9583a52fe476a7d95a4edeeac6becd763c84d791
vinybergamo <vinybergamo@gmail.com>
2025-03-26 12:56:47 -0300

    feat: add subaccount and splits properties to ChargeCreateBody interface

--- lib/interfaces/charge-service.interface.ts
+++ lib/interfaces/charge-service.interface.ts
@@ -23,10 +23,18 @@ export interface ChargeCreateBody {
   }>;
   enableCashbackPercentage?: boolean;
   enableCashbackExclusivePercentage?: boolean;
+  subaccount?: string;
+  splits?: Split[];
 }
 
 export interface ChargeRefundCreateBody {
   correlationID: string;
   value?: number;
   comment?: string;
+}
+
+interface Split {
+  value: number;
+  pixKey: string;
+  splitType?: 'SPLIT_INTERNAL_TRANSFER' | 'SPLIT_SUB_ACCOUNT' | 'SPLIT_PARTNER';
 }
